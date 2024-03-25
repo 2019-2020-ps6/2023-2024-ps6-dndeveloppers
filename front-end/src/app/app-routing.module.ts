df7c0b2c217927a8e79be34b3a81bf0074e476fd
Theo Cholewa <theo.cholewa@etu.univ-cotedazur.fr>
2024-03-25 09:12:31 +0100

    #1 feat component profil

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilComponent } from './profil/profil.component';
+const routes: Routes = [
+  {path: 'app-profil', component: ProfilComponent}
+];
 
-const routes: Routes = [];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
