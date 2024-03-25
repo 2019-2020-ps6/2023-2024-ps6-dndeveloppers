63e2221696e0eb68ad8bb12403d08c26269ea991
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-03-25 09:37:07 +0100

    #4 feat ajout du bouton d'indice

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfilComponent } from './profil/profil.component';
+import { IndiceComponent } from './quiz/indice/indice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfilComponent
+    ProfilComponent,
+    IndiceComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/quiz/indice/indice.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-indice',
+    templateUrl: './indice.component.html',
+    styleUrls: ['./indice.component.scss']
+})
+
+export class IndiceComponent implements OnInit {
+    constructor(){}
+
+    ngOnInit(): void {}
+}
