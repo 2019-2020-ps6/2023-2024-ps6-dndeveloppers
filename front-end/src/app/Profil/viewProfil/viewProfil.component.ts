9b78ed3ce0242c485bf22fcc045ef899e9c4384e
Theo Cholewa <theo.cholewa@etu.univ-cotedazur.fr>
2024-04-08 11:02:00 +0200

    #34 feat base ajout profil, lien profil, liste profils

---
+++ front-end/src/app/Profil/viewProfil/viewProfil.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-viewProfil',
+    templateUrl: './viewProfil.component.html',
+    styleUrls: ['./viewProfil.component.scss']
+})
+
+export class ViewProfilComponent implements OnInit {
+    constructor(){}
+
+    ngOnInit(): void {}
+}
