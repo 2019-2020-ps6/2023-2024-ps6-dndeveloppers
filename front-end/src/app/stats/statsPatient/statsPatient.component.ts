d630397a7fa66779f405e1ca5b7bd166da2f55e4
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-04-13 22:19:32 +0200

    #41 feat Second jet d'implémentation de graphes -> Réussite

--- front-end/src/app/stats/statsPatient/statsPatient.component.ts
+++ front-end/src/app/stats/statsPatient/statsPatient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import * as Highcharts from 'highcharts'
 import { LISTE_PROFILS } from "src/mocks/profil-list.mock";
 import { Profil } from "src/models/profil.model";
 
@@ -13,12 +14,38 @@ export class StatsPatientComponent implements OnInit {
     public listePatient: Profil[] = LISTE_PROFILS;
     public actualPatient: Profil  = this.listePatient[0];
 
+    public options: any = {
+        Chart: {
+            type: 'area',
+            height: 700
+        },
+        title: {
+            text: 'Evolution'
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            categories: ['1', '2', '3'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        series: [{
+            name: 'QuizName',
+            data: [0, 1, 2]
+        }]
+    }
+
     constructor(){
         console.log(this.listePatient[0]);
         console.log(this.selectedPatient);
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        Highcharts.chart('container', this.options);
+    }
 
     selectedPatient(event: any) {
         let nomPatient: string = event.target.value;
