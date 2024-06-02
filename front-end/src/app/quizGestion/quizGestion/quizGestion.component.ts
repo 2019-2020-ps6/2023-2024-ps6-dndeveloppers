5be4d5afe794c4c07bcd313c921935ab3c0846b7
MaxenceRoques <maxence.roques@etu.univ-cotedazur.fr>
2024-06-02 18:40:33 +0200

    #115 ajout de la barre de recherche dans la gestion des quizs

--- front-end/src/app/quizGestion/quizGestion/quizGestion.component.ts
+++ front-end/src/app/quizGestion/quizGestion/quizGestion.component.ts
@@ -11,14 +11,27 @@ import { QuizService } from "src/services/quiz.service";
 export class QuizGestionComponent implements OnInit {
 
     public quizList: Quiz[] = [];
+    public searchTerm: string = '';
+
+
 
     constructor(public quizService: QuizService) {
         this.quizService.quizzes$.subscribe((quizList) => {
           this.quizList = quizList;
         });
-
         this.quizService.setUpTheme();
     }
 
     ngOnInit(): void {}
+
+    filterQuizs() {
+        if (this.searchTerm) {
+            return this.quizList.filter(quiz => 
+                quiz.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+                quiz.theme.toLowerCase().includes(this.searchTerm.toLowerCase())
+            );
+        } else {
+            return this.quizList;
+        }
+    }
 }
