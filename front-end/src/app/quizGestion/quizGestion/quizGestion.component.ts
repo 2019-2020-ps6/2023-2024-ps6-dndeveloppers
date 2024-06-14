36a9faad2220f593fdce8193d8ebba4945303a39
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-06-14 23:19:54 +0200

    #127 feat Refonte de l'interface des quiz de la gestion des quiz

--- front-end/src/app/quizGestion/quizGestion/quizGestion.component.ts
+++ front-end/src/app/quizGestion/quizGestion/quizGestion.component.ts
@@ -11,6 +11,8 @@ import { QuizService } from "src/services/quiz.service";
 export class QuizGestionComponent implements OnInit {
 
     public quizList: Quiz[] = [];
+    public themeList: String[] = [];
+    public themeListShow: String[] = this.themeList;
     public searchTerm: string = '';
 
 
@@ -20,6 +22,11 @@ export class QuizGestionComponent implements OnInit {
           this.quizList = quizList;
         });
         this.quizService.setUpTheme();
+
+        this.quizService.themeList$.subscribe((themeList) => {
+            this.themeList = themeList;
+            this.themeListShow = this.themeList;
+        })
     }
 
     ngOnInit(): void {}
@@ -33,5 +40,22 @@ export class QuizGestionComponent implements OnInit {
         } else {
             return this.quizList;
         }
+    }
+
+    filterQuizsByTheme(theme: String) {
+        let res = [];
+        for (let i=0; i<this.quizList.length; i++) {
+          if (this.quizList[i].name.toLowerCase().includes(this.searchTerm.toLowerCase())
+            || this.quizList[i].theme.toLowerCase().includes(this.searchTerm.toLowerCase())) {
+            if (this.quizList[i].theme == theme) {
+              res.push(this.quizList[i]);
+            }
+          }
+        }
+        return res;
+      }
+
+    quizShow(event: any) {
+        console.log("okok");
     }
 }
