bf09d3dca79d833687dc9033ecc374da3c28155a
HugoH666 <hugo.heilmann@etu.univ-cotedazur.fr>
2024-06-18 15:04:58 +0200

    #227 test Fonctionalité d'ordre des tests

--- front-end/src/app/stats/statsPatient/statsPatient.fixture.ts
+++ front-end/src/app/stats/statsPatient/statsPatient.fixture.ts
@@ -1,34 +1,8 @@
 import { E2EComponentFixture } from "e2e/e2e-component.fixture";
-import { Quiz } from "src/models/quiz.model";
 
 export class StatsPatientFixture extends E2EComponentFixture {
 
-    public quizzes: Quiz[] = [];
-
-    getOptions(patientName: string) {
-        switch (patientName) {
-            case 'Bois, Maurice':
-                return "Indice";
-            default:
-                return "";
-        }
-    }
-
-    getNbQuizDone(patientName: string) {
-        switch (patientName) {
-            case 'Bois, Maurice':
-                return 0;
-            default:
-                return 0;
-        }
-    }
-
-    getMeanScore(patientName: string) {
-        switch (patientName) {
-            case 'Bois, Maurice':
-                return 0;
-            default:
-                return 0;
-        }
+    getRouteToProfil() {
+        return this.page.getByRole('button', { name: 'Voir les profils' });
     }
 }
--- front-end/src/app/stats/statsQuiz/statsQuiz.fixture.ts
+++ front-end/src/app/stats/statsQuiz/statsQuiz.fixture.ts
@@ -3,44 +3,6 @@ import { Quiz } from "src/models/quiz.model";
 
 export class StatsQuizFixture extends E2EComponentFixture {
 
-    public quizzes: Quiz[] = [];
-
-    getPlayedTime(quizName: string) {
-        switch (quizName) {
-            case 'Calcul mental':
-                return 0;
-            default:
-                return 0;
-        }
-    }
-
-    getMeanScore(quizName: string) {
-        switch (quizName) {
-            case 'Calcul mental':
-                return 0 + "/" + this.getNbQuestions(quizName);
-            default:
-                return 0 + "/" + 0;
-        }
-    }
-
-    getMeanHintUsed(quizName: string) {
-        switch (quizName) {
-            case 'Calcul mental':
-                return 0;
-            default:
-                return 0;
-        }
-    }
-
-    getNbQuestions(quizName: string) {
-        switch (quizName) {
-            case 'Calcul mental':
-                return 0;
-            default:
-                return 0;
-        }
-    }
-
     getRouteToQuiz() {
         return this.page.getByRole('button', { name: 'Aller modifier le quiz' });
     }
