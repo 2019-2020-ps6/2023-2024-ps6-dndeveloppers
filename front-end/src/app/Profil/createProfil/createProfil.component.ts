06eac63a912c87e471411df48883cff1f5482b34
Theo Cholewa <theo.cholewa@etu.univ-cotedazur.fr>
2024-06-18 09:50:41 +0200

    #244 feat import inutile

--- front-end/src/app/Profil/createProfil/createProfil.component.ts
+++ front-end/src/app/Profil/createProfil/createProfil.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from "@angular/router";
-import { skip } from "rxjs";
 import { PROFIL_NULL } from "src/mocks/profil.mock";
 import { STATS_PATIENT_INIT } from "src/mocks/statsMocks/stats-patient.mock";
 
