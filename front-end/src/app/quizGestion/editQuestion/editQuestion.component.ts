439ca7fc31a012e2755398d64c5dd464bd873577
Theo Cholewa <theo.cholewa@etu.univ-cotedazur.fr>
2024-06-19 19:35:16 +0200

    #273 feat pop up sans autres éléments

--- front-end/src/app/quizGestion/editQuestion/editQuestion.component.ts
+++ front-end/src/app/quizGestion/editQuestion/editQuestion.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Question_Model, Indice_Model1, Indice_Model2, Indice_Model3, QUIZ_LIST } from "src/mocks/quiz-list.mock";
-import { Indice } from "src/models/question.models";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Question_Model, Indice_Model} from "src/mocks/quiz-list.mock";
 import { Answer_Model } from "src/mocks/quiz-list.mock";
 import { Answer, Question } from "src/models/question.models";
 import { QuizService } from "src/services/quiz.service";
@@ -116,23 +115,23 @@ export class EditQuestionComponent implements OnInit {
     }
 
     editQuestion(){
-        let question : Question = JSON.parse(JSON.stringify(Question_Model));;        
+        let question : Question = JSON.parse(JSON.stringify(Question_Model));        
         question.label = this.questionForm.value.label;
 
         // réponses
-        let answer1 : Answer = JSON.parse(JSON.stringify(Answer_Model));;
+        let answer1 : Answer = JSON.parse(JSON.stringify(Answer_Model));
         answer1.value = this.questionForm.value.q1;
         answer1.isCorrect = false;
 
-        let answer2 : Answer = JSON.parse(JSON.stringify(Answer_Model));;
+        let answer2 : Answer = JSON.parse(JSON.stringify(Answer_Model));
         answer2.value = this.questionForm.value.q2;
         answer2.isCorrect = false;
 
-        let answer3 : Answer = JSON.parse(JSON.stringify(Answer_Model));;
+        let answer3 : Answer = JSON.parse(JSON.stringify(Answer_Model));
         answer3.value = this.questionForm.value.q3;
         answer3.isCorrect = false;
 
-        let answer4 : Answer = JSON.parse(JSON.stringify(Answer_Model));;
+        let answer4 : Answer = JSON.parse(JSON.stringify(Answer_Model));
         answer4.value = this.questionForm.value.q4;
         answer4.isCorrect = false;
 
@@ -147,45 +146,20 @@ export class EditQuestionComponent implements OnInit {
             }
         }
 
-        let indice1 : Indice = Indice_Model1;
-        if (this.questionForm.value.i1 == "indice 1") {
-            indice1.value = "";
-        } else {
-            indice1.value = this.questionForm.value.i1;
+        question.indice = [];
+        let indice = JSON.parse(JSON.stringify(Indice_Model));
+        if(this.questionForm.value.i1 != "" && this.questionForm.value.i1 != " "){
+            indice.value = this.questionForm.value.i1;
+            question.indice.push(indice);
         }
-
-        let indice2 : Indice = Indice_Model2;
-        if (this.questionForm.value.i2 == "indice 2") {
-            indice2.value = "";
-        } else {
-            indice2.value = this.questionForm.value.i2;
+        if(this.questionForm.value.i2 != "" && this.questionForm.value.i2 != " "){
+            indice.value = this.questionForm.value.i2;
+            question.indice.push(indice);
         }
-
-        let indice3 : Indice = Indice_Model3;
-        if (this.questionForm.value.i3 == "indice 3") {
-            indice3.value = "";
-        } else {
-            indice3.value = this.questionForm.value.i3;
+        if(this.questionForm.value.i3 != "" && this.questionForm.value.i3 != " "){
+            indice.value = this.questionForm.value.i3;
+            question.indice.push(indice);
         }
-
-        question.indice = [indice1, indice2, indice3];
-
-        if (indice1.value == "" && indice2.value != "" && indice3.value == "") {
-            indice1.value = indice2.value;
-            indice2.value = "";
-        } else if (indice1.value == "" && indice2.value == "" && indice3.value != "") {
-            indice1.value = indice3.value;
-            indice3.value = "";
-        } else if (indice1.value != "" && indice2.value == "" && indice3.value != "") {
-            indice2.value = indice3.value;
-            indice3.value = "";
-        } else if (indice1.value == "" && indice2.value != "" && indice3.value != "") {
-            indice1.value = indice2.value;
-            indice2.value = indice3.value;
-            indice3.value = "";
-        }
-
-        question.indice = [indice1, indice2, indice3];
 
         // photo
         if(this.photo != "" && this.questionForm.value.photoTexte != null && this.questionForm.value.photoTexte != "none"){
